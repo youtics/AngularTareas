561388e0f784560962d0d08cafa0ae64f4b557de
youtics <youtics@gmail.com>
2022-07-09 19:19:04 -0300

    Tareas Terminado

--- src/app/components/tareas/tareas.component.ts
+++ src/app/components/tareas/tareas.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Tarea } from 'src/app/models/Tarea';
 
 @Component({
   selector: 'app-tareas',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TareasComponent implements OnInit {
 
+  listaTareas: Tarea[] = [];
+  nombreTarea:string='';
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  agregarTarea()
+  {
+    const tarea = new Tarea (this.nombreTarea,false);
+
+    this.listaTareas.push(tarea);
+
+    this.nombreTarea='';
+  }
+
+  eliminarTarea(indice:number)
+  {
+    this.listaTareas.splice(indice,1);
+  }
+  
+  actualizarTarea(tarea:Tarea, i:number)
+  {
+    this.listaTareas[i].estado=!tarea.estado;
+  }
 }
